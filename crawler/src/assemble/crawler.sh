523b6f0a16c0ce9693c8386aa8f1ca2c7a3542cb
Enno <ennomeijers@protonmail.com>
2020-03-11 21:41:50 +0100

    Added config for logging level

--- crawler/src/assemble/crawler.sh
+++ crawler/src/assemble/crawler.sh
@@ -7,4 +7,4 @@ do
 done
 export CLASSPATH=$CLASSPATH
 
-java -Djsse.enableSNIExtension=false -Dsun.net.inetaddr.ttl=0 -Xmx16G -cp classes:$CLASSPATH eu.europeana.commonculture.lod.crawler.CommandLineInterface $*
+java -Djsse.enableSNIExtension=false -Djava.util.logging.config.file="logging.properties" -Dsun.net.inetaddr.ttl=0 -Xmx16G -cp classes:$CLASSPATH eu.europeana.commonculture.lod.crawler.CommandLineInterface $*
